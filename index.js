c621e635c5f328e49449b34f8fa959568f4219ad
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-06 22:42:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  if(line.length() == 0) {
+  if(line.length == 0) {
     console.log("There is nobody waiting to be served!")
   } else {
     return `Currently serving ${line[0]}.`
