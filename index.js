581c2c64ebdd7ad0002168498551726e5bdc33c8
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-06 22:36:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`
 
 }
+
+function nowServing(line) {
+  if(line.length == 0) {
+    console.log("There is nobody waiting to be served!")
+  } else {
+    return line[0]
+    line.shift()
+  }
+}
