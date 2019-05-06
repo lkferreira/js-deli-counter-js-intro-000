cbcd0ab79dcf88a8ccc811fcbc333a9cfd8d2ef8
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-06 23:16:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line) {
     return "The line is currently empty."
   } else {
     for (var i = 0; i < line.length; i++) {
-      if (i = line.length - 1) {
+      if (i == line.length - 1) {
         msg = msg + `${i + 1}. ${line[i]}`
       } else {
         msg = msg + `${i + 1}. ${line[i]}, `
