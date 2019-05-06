6801ad06a1c53232575e7e1d5bcc67b1eb0f3e42
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-06 23:09:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(line) {
 
 function currentLine(line) {
   var msg = 'The line is currently: '
-  
+
   if(line.length == 0) {
     return "The line is currently empty."
   } else {
